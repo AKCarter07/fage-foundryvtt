d655c45f17c22fe8b6adb3dd3e22aa846bde59b9
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-10-24 09:54:20 -0700

    got partials partially working

--- fage.js
+++ fage.js
@@ -2,18 +2,20 @@ import { fage } from "./modules/config.js";
 import fageItem from "./modules/sheets/fageItem.js";
 import fageActor from "./modules/sheets/fageActor.js";
 
-async function preloadHbsTemplates() {
+function preloadHandlebarsTemplates() {
+    console.log("FAGE | in preloadHandlebarsTemplates");
     const templatePaths = [
         "systems/fage/templates/partials/stats.hbs"
     ];
     return loadTemplates(templatePaths);
-}
+};
 
 
 Hooks.once("init", function(){
     console.log("FAGE | Initialising FAGE system");
 
     CONFIG.fage = fage;
+    preloadHandlebarsTemplates();
 
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("fage", fageItem, {makeDefault: true});
@@ -21,5 +23,5 @@ Hooks.once("init", function(){
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("fage", fageActor, {makeDefault: true});
 
-    preloadHbsTemplates;
-})
+    
+});
